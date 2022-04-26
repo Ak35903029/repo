68af3bba8bffb6031db56ae0cdf8ad7677b4654f
Abdussamad Khan <Abdussamad Khan@khans-desktop29>
2022-04-26 22:18:28 +0530

    first commit

---
+++ lol/src/lol/hub.java
@@ -0,0 +1,13 @@
+package lol;
+
+public class hub {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("king");
+		int x = 190;
+		System.out.println(x);
+
+	}
+
+}
